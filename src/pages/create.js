cdbcda83855922a43b7f50009a4a07d57b82f19d
William McGonagle <wpmcgonagle@gmail.com>
2022-12-26 10:57:58 -0500

    Added More Detail to Standards Create

--- src/pages/create.js
+++ src/pages/create.js
@@ -11,13 +11,42 @@ import clean from '../utility/stdCleaner.js';
 
 const example = `
 <head>
-   <tag>Testing</tag>
-   <tag>Example</tag>
+    <tag>Testing</tag>
+    <tag>Example</tag>
+    <version>1.0.0</version>
+    <author github="william-mcgonagle" email="mcgonaglew@fairfieldprogramming.org">
+        William McGonagle
+    </author>
 </head>
 
-# Title Here
+# Example Title
 
-The main focus of the software is to blah blah blah blah.
+This is a short description of what the standard is all about. You can write one or two sentences about it!
+
+## Purpose
+
+We recommend adding a few paragraphs about the purpose of the standard so that people understand what you are talking about. All of these paragraphs will be added to the about page of your standard so that people understand better before reading.
+
+### Inspiration or Past Standards
+
+You can write about ideas, documents, or past standards that may have led to this standard.
+
+### For use with XYZ
+
+Here, you can include some sections for what your standard should be used with.
+
+## Behavior
+
+## Architecture
+
+## Measurable Characteristics
+
+## Policy/ Management Requirements
+
+## Examples
+
+## Confirmation Tests
+
 `;
 
 function CreatePage() {
@@ -29,7 +58,7 @@ function CreatePage() {
 
     return (
         <Layout>
-            <section class="w-screen h-screen flex flex-row">
+            <section class="w-screen h-screen flex flex-row border-y-2">
                 <div class="w-1/2 h-full">
                     <textarea class="w-full h-full bg-slate-100 outline-0 p-2" onChange={(e) => { setData(e.target.value) }}>{data}</textarea>
                 </div>
